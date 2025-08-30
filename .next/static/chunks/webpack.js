c62cbd110f163868b555f5369b97b1b84955cc26
Daniel Kemelmakher <danielkemelmakher@MacBook-Pro-Daniel.local>
2025-08-30 22:45:34 +0300

    Prep for Vercel: scripts, /api/health, redirect, env example

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "54d9f8af78708d40"; }
+/******/ 		__webpack_require__.h = function() { return "46dac647cbe85716"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
